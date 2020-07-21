d597d375607377a3f00c744afeb73a48b9047bb3
vyas00 <vyasamrit@gmail.com>
2020-07-22 00:26:30 +0530

    Runtime permission with dexter

--- app/src/main/java/com/example/android/himusic/MainActivity.java
+++ app/src/main/java/com/example/android/himusic/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.android.himusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +22,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listViewForSongs=(ListView) findViewById(R.id.song_listView);
+        listViewForSongs=findViewById(R.id.song_listView);
+
+        runTimePermission();
 
 
+    }
+
+    public void runTimePermission()
+    {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                 .withListener(new PermissionListener() {
+                     @Override
+                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                     }
+
+                     @Override
+                     public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                     }
+
+                     @Override
+                     public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                    permissionToken.continuePermissionRequest();
+                     }
+                 }).check();
     }
 
 }
