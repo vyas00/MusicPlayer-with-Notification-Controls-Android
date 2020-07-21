8d3c9fdb8ffee8014c41f60ee2418d2a9c9ba258
vyas00 <vyasamrit@gmail.com>
2020-07-22 00:12:44 +0530

    ListView created in main activity_main.xml

--- app/src/main/java/com/example/android/himusic/MainActivity.java
+++ app/src/main/java/com/example/android/himusic/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.android.himusic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listViewForSongs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listViewForSongs=(ListView) findViewById(R.id.song_listView);
+
 
     }
+
 }
